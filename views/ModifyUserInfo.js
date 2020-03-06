83ca6e6730cf1cb8f62d9ec307d5149ec6a5e6e2
Vy Nguyen <vyn@metropolia.fi>
2020-03-06 18:08:28 +0200

    delete unnecessary files

--- views/ModifyUserInfo.js
+++ views/ModifyUserInfo.js
@@ -100,7 +100,7 @@ const ModifyUserInfo = props => {
     if (
       errors.username !== undefined ||
       errors.password !== undefined ||
-      errors.email !== undefined
+      errors.email !== undefined 
     ) {
       setSend(false);
     } else {
@@ -139,6 +139,7 @@ const ModifyUserInfo = props => {
               secureTextEntry={true}
               value={inputs.password}
               onChangeText={handlePassword}
+              
               error={errors.password}
             />
           </Item>
@@ -167,7 +168,7 @@ const ModifyUserInfo = props => {
           <AsyncImage
             style={{
               width: "100%",
-              height: deviceHeight / 2
+              height: deviceHeight / 2.5
             }}
             spinnerColor="#777"
             source={{ uri: avatar }}
